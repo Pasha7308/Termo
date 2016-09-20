ee7bb46631a0fd7c0abf41803f2dbae8ba1c7ff8
pasha7308 <pasha7308@mail.ru>
2016-09-20 21:49:45 +0700

    Notification added

--- app/src/main/java/com/pasha/termo/DownloadWebpageService.java
+++ app/src/main/java/com/pasha/termo/DownloadWebpageService.java
@@ -1,16 +1,18 @@
 package com.pasha.termo;
 
+import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProviderInfo;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
 import android.os.AsyncTask;
-import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.widget.RemoteViews;
@@ -20,14 +22,16 @@ import com.pasha.termo.utils.Colorer;
 import com.pasha.termo.utils.DateUtils;
 import com.pasha.termo.utils.TempRounder;
 
+import static android.support.v4.app.NotificationCompat.VISIBILITY_PUBLIC;
+
 public class DownloadWebpageService extends AsyncTask<Object, Integer, WeatherDto>
 {
     private static final String LOG = "DownloadWebpageService";
-    protected Intent intent;
-    protected Context context;
-    protected Class callerClass;
-    protected int wPx = 0;
-    protected int hPx = 0;
+    private Intent intent;
+    private Context context;
+    private Class callerClass;
+    private int wPx = 0;
+    private int hPx = 0;
 
     protected WeatherDto doInBackground(
         Object... objs)
@@ -82,9 +86,7 @@ public class DownloadWebpageService extends AsyncTask<Object, Integer, WeatherDt
             } else {
                 bm = Bitmap.createBitmap(is1x1 ? 192 : 192 * 2, 192, Bitmap.Config.ARGB_8888);
             }
-            Canvas canvas = new Canvas(bm);
-            DrawView drawView = new DrawView(true);
-            drawView.draw(canvas, dto.getOldValues());
+            drawOnBitmap(bm, dto, true);
             remoteViews.setBitmap(R.id.imgvWidget, "setImageBitmap", bm);
 
             // Register an onClickListener
@@ -102,6 +104,7 @@ public class DownloadWebpageService extends AsyncTask<Object, Integer, WeatherDt
 
             appWidgetManager.updateAppWidget(widgetId, remoteViews);
         }
+        addNotification(dto);
     }
 
     private void getSizes(int appWidgetId)
@@ -115,8 +118,61 @@ public class DownloadWebpageService extends AsyncTask<Object, Integer, WeatherDt
         hPx = dpToPx(h_dp, context);
     }
 
-    public static int dpToPx(int dp, Context context)
+    private static int dpToPx(int dp, Context context)
     {
         DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
         return Math.round(dp * displayMetrics.density);
-    }}
+    }
+
+    private void drawOnBitmap(Bitmap bm, WeatherDto dto, boolean isWidget) {
+        Canvas canvas = new Canvas(bm);
+        DrawView drawView = new DrawView(isWidget);
+        drawView.draw(canvas, dto.getOldValues());
+    }
+
+    private void addNotification(WeatherDto dto) {
+        Resources res = context.getResources();
+        String caption = res.getString(R.string.strCaption);
+        String tempTermo = TempRounder.round(dto.getServerTermo().getTemp(), false, false);
+        String tempIao = TempRounder.round(dto.getServerIao().getTemp(), false, true);
+        String text = String.format(res.getString(R.string.strNotificationTemplate), tempTermo, tempIao);
+        String textTermo = String.format(res.getString(R.string.strNotificationTemplateTermo), tempTermo);
+        String textIao = String.format(res.getString(R.string.strNotificationTemplateIao), tempIao);
+        Bitmap bm = Bitmap.createBitmap(256, 256, Bitmap.Config.ARGB_8888);
+        drawOnBitmap(bm, dto, false);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle(caption)
+                        .setContentText(text)
+                        .setLargeIcon(bm)
+                        .setAutoCancel(false);
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+        inboxStyle.setBigContentTitle(caption);
+        mBuilder.setStyle(inboxStyle);
+        inboxStyle.addLine(textTermo);
+        inboxStyle.addLine(textIao);
+
+        mBuilder.setVisibility(VISIBILITY_PUBLIC);
+
+// Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(context, MainActivity.class);
+// The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+// Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity.class);
+// Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+            (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+        int notificationId = res.getInteger(R.integer.notificationId);
+        mNotificationManager.notify(notificationId, mBuilder.build());
+    }
+}
