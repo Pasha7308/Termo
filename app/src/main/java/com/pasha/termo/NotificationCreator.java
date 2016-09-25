be42d1bcd57dbd3d2d36dfb17b2113c79cadefd6
pasha7308 <pasha7308@mail.ru>
2016-09-25 16:47:54 +0700

    Custom Notification Layouts

--- app/src/main/java/com/pasha/termo/NotificationCreator.java
+++ app/src/main/java/com/pasha/termo/NotificationCreator.java
@@ -7,11 +7,13 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.support.v4.app.NotificationCompat;
+import android.widget.RemoteViews;
 
 import com.pasha.termo.model.WeatherDto;
+import com.pasha.termo.utils.Colorer;
+import com.pasha.termo.utils.DateUtils;
 import com.pasha.termo.utils.TempRounder;
 
-import static android.support.v4.app.NotificationCompat.VISIBILITY_PUBLIC;
 
 class NotificationCreator {
     private Context context;
@@ -22,29 +24,30 @@ class NotificationCreator {
 
     void addNotification(WeatherDto dto) {
         Resources res = context.getResources();
+
         String tempTermo = TempRounder.round(dto.getServerTermo().getTemp(), false, false);
         String tempIao = TempRounder.round(dto.getServerIao().getTemp(), false, true);
-        String textTermo = String.format(res.getString(R.string.strNotificationTemplateTermo), tempTermo);
-        String textIao = String.format(res.getString(R.string.strNotificationTemplateIao), tempIao);
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.notification);
+        remoteViews.setTextViewText(R.id.lblNotTextTermo, tempTermo);
+        remoteViews.setTextColor(R.id.lblNotTextTermo, Colorer.getColorOutOfTemp(tempTermo));
+
+        remoteViews.setTextViewText(R.id.lblNotTextIao, tempIao);
+        remoteViews.setTextColor(R.id.lblNotTextIao, Colorer.getColorOutOfTemp(tempIao));
+
+        remoteViews.setViewVisibility(R.id.lblNotTime, (dto.getUpdated().GetDateTime() != null) ? 1 : 0);
+        if (dto.getUpdated().GetDateTime() != null) {
+            remoteViews.setTextViewText(R.id.lblNotTime, DateUtils.timeToString(dto.getUpdated().GetDateTime(), false));
+        }
+
         Bitmap bm = Bitmap.createBitmap(256, 256, Bitmap.Config.ARGB_8888);
-        DrawManager.drawOnBitmap(bm, dto, false);
+        DrawManager.drawOnBitmap(bm, dto, true);
+        remoteViews.setBitmap(R.id.imgvNot, "setImageBitmap", bm);
 
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(context)
                         .setSmallIcon(R.drawable.ic_stat_notification)
-                        .setContentTitle(textTermo)
-                        .setContentText(textIao)
-                        .setLargeIcon(bm)
+                        .setContent(remoteViews)
                         .setAutoCancel(false);
-/* more complex notification. Not necessary because there is no additional information
-        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
-        inboxStyle.setBigContentTitle(caption);
-        mBuilder.setStyle(inboxStyle);
-        inboxStyle.addLine(textTermo);
-        inboxStyle.addLine(textIao);
-*/
-
-        mBuilder.setVisibility(VISIBILITY_PUBLIC);
 
 // Creates an explicit intent for an Activity in your app
         Intent resultIntent = new Intent(context, MainActivity.class);
